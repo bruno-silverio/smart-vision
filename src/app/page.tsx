44cc08e9da6efeac0be68276da04a6e9d28b01c6
bruno-silverio <bcamilo94@gmail.com>
2024-07-15 22:34:30 -0300

    chore: add heroicons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import Header from "./components/Header";
 
+import { InformationCircleIcon } from '@heroicons/react/24/outline'
+import { PlayIcon } from '@heroicons/react/24/solid'
+
 export default function Home() {
   return (
     <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]'>
@@ -28,9 +31,11 @@ export default function Home() {
 
         <div className='flex space-x-4'>
           <button className='md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
+            <PlayIcon className="size-6 text-black" />
             Detection
           </button>
           <button className='md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
+            <InformationCircleIcon className="size-6 text-black" />
             More Info
           </button>
         </div>
